8a8bbfc83fc40db837cb0e04798ea48e5d46f94d
Samadrita Bhattacharya <136957481+SamadritaBhattacharya@users.noreply.github.com>
2023-08-21 14:32:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+window.onload = function(){
+
+    L.mapquest.key = 'rSiqvvMoywE3G0EyNcrRfPzhxwA3GAo5'; 
+
+    const map = L.mapquest.map('map', {
+    center: [37.7749, -122.4194],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 12
+    });
+
+    map.addControl(L.mapquest.control({ position: "bottomright" }));
+
+    // //map.addControl(L.mapquest.control());
+
+
+     L.marker([53.480759, -2.242631], {
+            icon: L.mapquest.icons.marker({
+            primaryColor: '#22407F',
+            secondaryColor: '#3B5998',
+            shadow: true,
+            size: 'md',
+            symbol: 'A'
+            })
+    })
+    .bindPopup('This is Manchester!')
+    .addTo(map);
+    L.mapquest.geocodingControl().addTo(map);
+
+}
+
